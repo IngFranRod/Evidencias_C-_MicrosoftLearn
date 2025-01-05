47db4866be94d6f29b0032015a2e0451ff85bfd2
Franyany Rodriguez Cabrera <123431540+IngFranRod@users.noreply.github.com>
2025-01-04 21:07:19 -0400

    Add files via upload

---
+++ 20-Myclient/Program.cs
@@ -0,0 +1,16 @@
+﻿using CodeLibrary;
+
+namespace MyClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapWebpage("https://msdn.microsoft.com");
+            Console.Write(value);
+
+        }
+    }
+
+}
