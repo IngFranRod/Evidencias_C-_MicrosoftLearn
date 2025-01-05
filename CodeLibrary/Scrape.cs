44c44866f00fb52f65d5b75186a023b55b9ed15e
Franyany Rodriguez Cabrera <123431540+IngFranRod@users.noreply.github.com>
2025-01-04 21:06:50 -0400

    Add files via upload

---
+++ CodeLibrary/Scrape.cs
@@ -0,0 +1,27 @@
+﻿using System.Net;
+
+namespace CodeLibrary
+{
+    public class Scrape
+    {
+
+        public string ScrapWebpage(string url)
+        {
+           return GetWebpage(url);
+        }
+
+        public string ScrapWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            return client.DownloadString(url);
+        }
+    }
+}
