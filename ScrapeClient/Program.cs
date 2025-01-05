1c1ee793add83797e216d2225fc6674d58f974b9
Franyany Rodriguez Cabrera <123431540+IngFranRod@users.noreply.github.com>
2025-01-04 21:08:00 -0400

    Add files via upload

---
+++ ScrapeClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using ScrapeLibrary;
+
+namespace ScrapeClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Scrape myScrape = new Scrape();
+            string value = myScrape.ScrapWebpage("https://msdn.microsoft.com");
+            Console.Write(value);
+
+        }
+    }
+}
---
+++ ScrapeLibrary/Scrape.cs
@@ -0,0 +1,28 @@
+﻿using System.Net;
+
+namespace ScrapeLibrary
+{
+    public class Scrape
+    {
+        public string ScrapWebpage(string url)
+        {
+            return GetWebpage(url);
+        }
+
+        public string ScrapWebpage(string url, string filepath)
+        {
+            string reply = GetWebpage(url);
+
+            File.WriteAllText(filepath, reply);
+            return reply;
+        }
+
+        private string GetWebpage(string url)
+        {
+            WebClient client = new WebClient();
+            string content = client.DownloadString(url);
+            content += "THAT's ALL FOLKS!!!";
+            return content;
+        }
+    }
+}
