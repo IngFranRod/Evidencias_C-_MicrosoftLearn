0bed6a67c70ee889cd7382056d6b430ad1cdba1c
Franyany Rodriguez Cabrera <123431540+IngFranRod@users.noreply.github.com>
2025-01-04 21:07:32 -0400

    Add files via upload

---
+++ 21-Namespace/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace namespaces
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            
+           
+            WebClient client = new WebClient();
+            string reply = client.DownloadString("Http://msdn.microsoft.com");
+
+            Console.WriteLine(reply);
+
+            //gets full page html to file
+            File.WriteAllText(@"C:\Users\frany\OneDrive\Escritorio\Learn\21-Namespace\WriteText.txt", reply);
+
+            Console.ReadLine();
+        }
+    }
+}
